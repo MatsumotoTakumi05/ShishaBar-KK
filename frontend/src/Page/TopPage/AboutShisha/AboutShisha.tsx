c4c613fe65e2481bd47cee750ca12b03e9e86d45
松本卓巳 <matsu_one_taku@icloud.com>
2024-10-20 20:22:55 +0900

    update_シーシャの説明ページを作成

--- frontend/src/Page/TopPage/AboutShisha/AboutShisha.tsx
+++ frontend/src/Page/TopPage/AboutShisha/AboutShisha.tsx
@@ -1,18 +1,32 @@
 import { Box, Button, Container, Typography } from '@mui/material'
 import React, { memo } from 'react'
 import CustomLayout from '../../../Component/modules/Layout/CustomLayout'
+import { useNavigate } from 'react-router-dom'
 
 const AboutShisha = memo(() => {
+    // navigateの定義
+    const navigate = useNavigate()
+    // 外郭のcss変数
+    const bgImageStyle = [{
+        backgroundImage: "url(./AboutShisha.jpeg)",
+        backgroundSize: "100% 100%",
+    }]
+
+    const detailPageNaviOnClick = () => {
+        navigate("/KK/AboutShisha")
+    }
+
+
     return (
-        <CustomLayout tittleText="What's Shisha">
-            <Container maxWidth="md" sx={{ display: "flex", width: "100%", backgroundImage: "./ShishaAbout_BgImage.jpeg" }}>
-                <Box sx={{ width: "30%" }}>
-                    <Typography>
-                        シーシャは、水タバコの一種で、香り豊かなフレーバーの煙をゆっくりと吸い込む嗜好品です。<br />
+        <CustomLayout tittleText="What's Shisha" backgroundBorder={bgImageStyle}>
+            <Container maxWidth="md" sx={{ display: "flex", width: "100%", }}>
+                <Box sx={{ width: "50%", padding: "30px" }} >
+                    <Typography sx={{ fontSize: "24px" }}>
+                        シーシャは、水タバコの一種で、香り豊かなフレーバーの煙をゆっくりと吸い込む嗜好品です。
                         フルーツやミントなどのフレーバー付きタバコ葉を炭で加熱し、水を通して吸うため、煙がまろやかになるのが特徴です。
                         数多くのフレーバーの中からお気に入りを見つけ、ぜひリラックスしたひと時をお過ごしください。
                     </Typography>
-                    <Button variant="outlined">Read More→</Button>
+                    <Button variant="outlined" sx={{ mt: "30px" }} onClick={detailPageNaviOnClick}> Read More→</Button>
                 </Box>
             </Container>
 
